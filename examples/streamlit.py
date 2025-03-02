a6d32962da0f426e9feed19acb3881ca753832a0
Helmut Hoffer von Ankershoffen <helmut@aignostics.com>
2025-03-02 12:26:26 +0100

    feat: various

--- examples/streamlit.py
+++ examples/streamlit.py
@@ -1,65 +1,30 @@
 """
-Streamlit web application that provides a simple interface for the Brave Search API.
+Streamlit web application that demonstrates a simple interface for OE Python Template.
 
-This module creates a web interface using Streamlit to demonstrate the usage of
-the Brave Search Python Client for performing web searches.
+This module creates a web interface using Streamlit to demonstrate the usage of the service provided by
+OE Python Template.
 """
 
-import asyncio
-
 import streamlit as st
-from brave_search_python_client import (
-    BraveSearch,
-    WebSearchApiResponse,
-    WebSearchRequest,
+
+from oe_python_template import (
+    Service,
     __version__,
 )
 
 sidebar = st.sidebar
-api_key = sidebar.text_input(
-    "[Brave Search API key](https://brave.com/search/api/)",
-    type="password",
-)
 sidebar.write(
-    f" [Brave Search Python Client v{__version__}](https://brave-search-python-client.readthedocs.io/)",
+    f" [OE Python Template v{__version__}](https://oe-python-template.readthedocs.io/)",
 )
 sidebar.write("Built with love in Berlin 🐻")
 
-st.title("🦁 Brave Search ")
-q = st.text_input("Query")
+st.title("🧠 OE Python Template ")
 
-if api_key and q:
-    # Initialize the BraveSearch client
-    bs = BraveSearch(api_key=api_key)
+# Initialize the service
+service = Service()
 
-    @st.cache_data
-    def search(q: str) -> WebSearchApiResponse:
-        """
-        Perform a synchronous web search using Brave Search API.
+# Get the message
+message = service.get_hello_world()
 
-        This function wraps an asynchronous API call into a synchronous interface by
-        using asyncio.run(). It executes a web search query through the Brave Search API.
-
-        Returns:
-            WebSearchApiResponse: The search results from the API.
-
-        """
-        return asyncio.run(bs.web(WebSearchRequest(q=q)))
-
-    # Perform the search
-    response = search(q)
-
-    # Print the response as JSON
-    cols = st.columns([0.1, 0.9])
-    with cols[0]:
-        st.write("JSON:")
-    with cols[1]:
-        st.json(response.model_dump(), expanded=False)
-
-    # Print the search results of type web
-    for result in response.web.results if response.web else []:
-        st.write(f"[{result.title}]({result.url})")
-elif q:
-    st.write(
-        "Please enter your [Brave Search API key](https://brave.com/search/api/) in the sidebar.",
-    )
+# Print the message
+st.write(f"{message}")
